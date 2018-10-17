b8a97948858a15f9d9558ae56a004c26a15216f9
SW <x774436248@gmail.com>
2018-10-17 17:38:09 +0800

    Modefied files 2018/10/17

--- js/shared.js
+++ js/shared.js
@@ -1,39 +1,44 @@
-$(document).ready(function () {
-    initialComponent()
+$(document).ready(function() {
+  initialComponent();
 });
-var initialComponent = function () {
-    $(".nav-link").click(function () {
-        $("#render_body").html("");
-        fnGetRenderbody(this);
-    })
-
-
-}
-var fnGetRenderbody = function (e) {
+var initialComponent = function() {
+  $(".nav-link").click(function() {
+    $("#render_body").html("");
+    fnGetRenderbody(this);
+  });
+};
+var fnGetRenderbody = function(e) {
+  if (!$(e).hasClass("noChild")) {
     $.ajax({
-        url: $(e).attr("id"),
-        dataType: "html"
-    }).done(function (content) {
-        $("#render_body").html(content);
-    })
-}
-var initDashBoard = function () {
-    var widthOfDashBoard = $("#dashBoard").width();
-    var animateDistance = -(widthOfDashBoard - 15).toString() + "px";
-    $("#db_button").click(function () {
-        if ($("#dashBoard").hasClass('db_open')) {
-            $("#dashBoard").animate({
-                "margin-left": animateDistance
-            }, "slow");
-            $("#dashBoard").removeClass('db_open');
-            $("#dashBoard").addClass('db_close');
-        }
-        else {
-            $("#dashBoard").animate({
-                "margin-left": "0px"
-            }, "slow");
-            $("#dashBoard").removeClass('db_close');
-            $("#dashBoard").addClass('db_open');
-        }
+      url: $(e).attr("id"),
+      dataType: "html"
+    }).done(function(content) {
+      $("#render_body").html(content);
     });
-}
+  }
+};
+var initDashBoard = function() {
+  var widthOfDashBoard = $("#dashBoard").width();
+  var animateDistance = -(widthOfDashBoard - 15).toString() + "px";
+  $("#db_button").click(function() {
+    if ($("#dashBoard").hasClass("db_open")) {
+      $("#dashBoard").animate(
+        {
+          "margin-left": animateDistance
+        },
+        "slow"
+      );
+      $("#dashBoard").removeClass("db_open");
+      $("#dashBoard").addClass("db_close");
+    } else {
+      $("#dashBoard").animate(
+        {
+          "margin-left": "0px"
+        },
+        "slow"
+      );
+      $("#dashBoard").removeClass("db_close");
+      $("#dashBoard").addClass("db_open");
+    }
+  });
+};
