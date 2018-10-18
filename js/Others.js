0e6f9f23633392e6237607b60224a5065752dd57
SW <x774436248@gmail.com>
2018-10-18 17:35:58 +0800

    Modefied files.

--- js/Others.js
+++ js/Others.js
@@ -1,8 +1,26 @@
-$(document).ready(function(){
-    initOthers();
+var rootDoc;
+$(document).ready(function() {
+  rootDoc = "Others";
+  initOthers();
 });
 
-var initOthers=function(){
-    initDashBoard();
-    $("#menu").menu();
-}
+var initOthers = function() {
+  initDashBoard();
+  $("#menu").menu();
+  $(".Menu_Item").click(function(e) {
+    var urlStr = rootDoc + "/";
+    var Nodes = $(e.currentTarget)
+      .closest('li[class^="Root_item"]')
+      .find("div");
+    $.each(Nodes, function(index, value) {
+      urlStr += value.innerText + "/";
+    });
+    urlStr=urlStr.substring(0,urlStr.length-1)+".html";        
+    $.ajax({
+      url: urlStr,
+      dataType: "html"
+    }).done(function(content) {
+      $("#bootStrap_body").html(content);
+    });
+  });
+};
